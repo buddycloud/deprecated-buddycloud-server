11afc035ebab07298684e5de8af851103a7f6b9f
Astro <astro@spaceboyz.net>
2011-01-10 23:16:44 +0100

    fixes

--- model_couchdb.js
+++ model_couchdb.js
@@ -1,3 +1,4 @@
+/* TODO: remove 409 'conflict' retrying from cradle */
 var cradle = require('cradle@0.3.1');
 cradle.setup({host: '127.0.0.1',
 	      port: 5984,
@@ -13,39 +14,63 @@ function Transaction(cb) {
     cb(null, this);
 }
 
+Transaction.prototype.commit = function(cb) {
+    cb(null);
+};
+
+
 Transaction.prototype.createNode = function(owner, node, cb) {
     /* TODO: filter node for chars */
-    db.save(encodeURIComponent(node), { }, cb);
+    db.save(encodeURIComponent(node), { }, function(err) {
+	cb(err && new Error(err.error));
+    });
 };
 
 Transaction.prototype.subscribeNode = function(subscriber, node, cb) {
     db.get(encodeURIComponent(node), function(err, res) {
+	if (err) {
+	    cb(new Error(err.error));
+	    return;
+	}
+
 	var doc = res && res.toJSON();
-	if (err)
-	    cb(err);
-	else if (!doc) {
+	if (!doc) {
 	    cb(new Error('not-found'));
 	} else {
-console.log({before:JSON.stringify(doc)});
 	    if (!doc.hasOwnProperty('subscribers'))
 		doc.subscribers = [];
 	    doc.subscribers.push(subscriber);
-console.log({after:JSON.stringify(doc)});
-	    db.save(encodeURIComponent(node), doc._rev, doc, cb);
+	    db.save(encodeURIComponent(node), doc._rev, doc, function(err) {
+		if (err && err.error === 'conflict')
+		    subscribeNode(subscriber, node, cb);
+		else
+		    cb(err && new Error(err.error));
+	    });
 	}
     });
 };
 
-Transaction.prototype.publishItems = function(publisher, node, items, cb) {
+Transaction.prototype.getSubscribers = function(node, cb) {
+    db.get(encodeURIComponent(node), function(err, res) {
+	if (err) {
+	    cb(err && new Error(err.error));
+	    return;
+	}
+
+	var doc = res && res.toJSON();
+	cb(null, doc.subscribers || []);
+    });
+};
+
+Transaction.prototype.writeItems = function(publisher, node, items, cb) {
     var docs = [];
     for(var id in items) {
 	if (items.hasOwnProperty(id))
-	    docs.push({ _id: node + '/' + id,
+	    docs.push({ _id: encodeURIComponent(node) + '/' + encodeURIComponent(id),
 			xml: items[id].toString() });
     }
-    db.save(docs, cb);
+    db.save(docs, function(err) {
+	cb(err && new Error(err));
+    });
 };
 
-Transaction.prototype.commit = function(cb) {
-    cb(null);
-};
