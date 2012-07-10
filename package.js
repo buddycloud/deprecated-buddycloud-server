b86546239d9e917912f4843bbc18713099d7b04c
dodo <dodo.the.last@gmail.com>
2012-07-10 11:10:47 +0200

    colorful output for package.js

--- package.js
+++ package.js
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
+require('colors')
 var fs = require('fs')
 var path = require('path')
 var tarballify = require('tarballify')
 var version = require('./lib/version')
 var name = require('./lib/name')
 
-console.log("creating new tarball …")
+console.log("creating new tarball …".green)
 var tarball = tarballify("./lib/main.js", {
     dirname:__dirname,
+    cache:false,
 })
-    .register(".node", function (body, file) {
-        console.log("skip binary file:", file)
-        return "skip binary"
-    })
-    .on('error', console.error.bind(console))
-    .on('wait', function(){console.log("waiting for tarball to finish …")})
-    .on('append', function(f,e){console.log("append",f.props.size,"\t",e.name, "\t",f.path)})
-    .on('close', function(){console.log("done.")})
+    .on('warn',  function(w){console.warn( "WARN".yellow,w)})
+    .on('error', function(e){console.error("ERR ".red   ,e)})
+    .on('skip',  function(s){console.log( "skip".bold.blue,s.name.cyan,s.dirname)})
+    .on('wait', function(){console.log("waiting for tarball to finish …".green)})
+//     .on('append', function(f,e){console.log("append",f.props.size,"\t",e.name, "\t",f.path)})
+    .on('close', function(){console.log("done.".bold.green)})
 //     .on('syntaxError', console.error.bind(console))
 tarball.pipe(fs.createWriteStream(path.join(__dirname, name+"-"+version+".tar.gz")))
 ;[
@@ -27,5 +27,5 @@ tarball.pipe(fs.createWriteStream(path.join(__dirname, name+"-"+version+".tar.gz
     "README.md"
 ].forEach(function(file){tarball.append(file)})
 
-console.log("setup ready …")
+console.log("setup ready …".green)
 tarball.end()
